8299d5b8708b642b650a7b60cf0afc667ca4fb70
xekiellc <zach@xekie.com>
2026-05-20 20:45:21 -0400

    Update config.js

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -8,7 +8,8 @@ exports.handler = async () => {
     body: JSON.stringify({
       supabaseUrl: process.env.SUPABASE_URL,
       supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
-      GIPHY_API_KEY: process.env.GIPHY_API_KEY
+      GIPHY_API_KEY: process.env.GIPHY_API_KEY,
+      RECAPTCHA_SITE_KEY: '6LeOqfQsAAAAAF1mZktRAvH850-95HxEzpfVoTZA'
     })
   };
 };
